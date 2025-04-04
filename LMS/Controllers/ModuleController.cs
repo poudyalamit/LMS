9defa87ea38374909fa65470ab7d48660f7dcca3
poudyalamit <poudyalamit20@gmail.com>
2025-04-04 10:24:02 +0545

    feat: module conteol

--- LMS/Controllers/ModuleController.cs
+++ LMS/Controllers/ModuleController.cs
@@ -1,12 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS.Data;
+using LMS.Models;
+using LMS.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.Controllers
 {
+    [Authorize(Roles = "Teacher")]
     public class ModuleController : Controller
     {
-        public IActionResult Index()
+        private readonly IModuleRepository _moduleRepo;
+        private readonly ApplicationDbContext _context;
+        public ModuleController(IModuleRepository moduleRepo, ApplicationDbContext context)
+        {
+            _moduleRepo = moduleRepo;
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int courseId)
+        {
+            var modules = await _context.Modules.Where(c => c.CourseId == courseId).ToListAsync();
+            return View(modules);
+        }
+
+        public IActionResult AddModule()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddModule(Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                await _moduleRepo.AddModule(module);
+                return RedirectToAction("Index", new { courseId = module.CourseId });
+            }
+            return View(module);
+        }
+
+        public async Task<IActionResult> EditModule(int id)
+        {
+            var module = await _moduleRepo.GetModuleById(id);
+            if (module == null)
+            {
+                return NotFound();
+            }
+            return View(module);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditModule(Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                await _moduleRepo.UpdateModule(module);
+                return RedirectToAction("Index", new { courseId = module.CourseId });
+            }
+            return View(module);
+        }
+
+        public async Task<IActionResult> DeleteModule(int id)
+        {
+            var module = await _moduleRepo.GetModuleById(id);
+            if (module == null)
+            {
+                return NotFound();
+            }
+            return View(module);
+        }
+        [HttpPost, ActionName("DeleteModule")]
+        public async Task<IActionResult> DeleteModuleConfirmed(int id)
+        {
+            var module = await _moduleRepo.GetModuleById(id);
+            if (module == null)
+            {
+                return NotFound();
+            }
+            await _moduleRepo.DeleteModule(module);
+            return RedirectToAction("Index", new { courseId = module.CourseId });
         }
     }
 }
